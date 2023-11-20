2276d5ab43c3bacfec7262479e83ba3acd266a66
Roy Huang <98927448+RoyH11@users.noreply.github.com>
2023-11-19 18:24:52 -0600

    stack

--- Solution.java
+++ Solution.java
@@ -1,48 +1,40 @@
 import java.util.*;
 
 class Solution {
-    public static List<String> summaryRanges(int[] nums) {
-        List<String> result = new ArrayList<>();
-        if(nums.length > 0){
-            for(int i=0; i<nums.length; i++){
-                
-                String current = Integer.toString(nums[i]);                
-                
+    public static boolean isValid(String s) {
 
-                if( ((i+1)<nums.length) && ((nums[i]+1) == nums[i+1]) ){
-                    
-                    // continuous
-                    current = current+"->";
-                    for(int j = i+1; j<nums.length; j++){
-                        
-                        if(nums[j]!=nums[j-1]+1){
-                            // continuous 
-                            current = current+ Integer.toString(nums[j-1]);
-                            i = j-1;
-                            result.add(current);
-                            break;
-                        }
-                    }
-                    if(current.charAt(current.length()-1) == '>'){
-                        current = current+ Integer.toString(nums[nums.length-1]);
-                        result.add(current);
-                        break;
-                    }
-                }else{
-                    
-                    // end single case or discontinuous
-                    result.add(current);
-                }
-            }
+        //check odd
+        if(s.length()%2 != 0){
+            return false;
         }
-        return result;
+
+        Stack<Character> stack = new Stack<>();
+        stack.push(s.charAt(0));
+
+        for(int i = 1; i<s.length(); i++){
+            char curr = s.charAt(i);
+            if(!stack.empty()){
+                char prev = stack.peek();
+
+                String pair = ""+prev+curr;
+
+                if(pair.equals("()") || pair.equals("[]") || pair.equals("{}")){
+                    stack.pop();
+                }else{
+                    stack.push(curr);
+                }
+            }else{
+                stack.push(curr);
+            }
+            
+        }
+
+        return stack.empty();
     }
 
     public static void main(String[] args){
 
-        int[] nums2 = {0,1,2,4,5,7};
-        System.out.println(summaryRanges(nums2));
-        int[] nums3 = {0,2,3,4,6,8,9};
-        System.out.println(summaryRanges(nums3));
+        String s = "()[]{}";
+        System.out.println(isValid(s));
     }
 }
