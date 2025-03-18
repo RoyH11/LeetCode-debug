a35ba7fd31cc6c197d404f00d5488c6d207e7eb8
Roy Huang <98927448+RoyH11@users.noreply.github.com>
2025-03-18 15:49:45 -0500

    Update leetcode150.py

--- leetcode150.py
+++ leetcode150.py
@@ -169,4 +169,69 @@ class Solution(object):
 
         return not stack
             
-        
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        visited = set()
+        while head: 
+            if head not in visited: 
+                visited.add(head)
+                head = head.next
+            else: 
+                return True
+            
+        return False
+    
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        slow, fast = head, head
+        while fast and fast.next: 
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast: 
+                return True
+            
+        return False
+    
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode()
+        tail = dummy 
+        while list1 and list2: 
+            if list1.val < list2.val: 
+                tail.next = list1
+                list1 = list1.next
+            else: 
+                tail.next = list2
+                list2 = list2.next
+
+            tail = tail.next
+
+        tail.next = list1 if list1 else list2
+
+        return dummy.next 
