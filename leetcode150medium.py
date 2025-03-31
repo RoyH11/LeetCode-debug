decb0f555dd72f543c8a504ef1a8c52586736d4c
RoyH11 <ruoqiuhuang@gmail.com>
2025-03-31 16:31:31 -0500

    189

--- leetcode150medium.py
+++ leetcode150medium.py
@@ -37,3 +37,43 @@ class Solution:
                 write += 1
                 
         return write 
+    
+# 189
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(k): 
+            nums[:] = nums[-1:] + nums[:-1]
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n
+        nums[:] = nums[-k:] + nums[:-k]
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n
+
+        nums.reverse()
+        nums[:k] = reversed(nums[:k])
+        nums[k:] = reversed(nums[k:])
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        total = 0 
+        for i in range(len(prices) - 1): 
+            if prices[i+1] > prices[i]: 
+                total += prices[i+1] - prices[i]
+
+        return total 
+
