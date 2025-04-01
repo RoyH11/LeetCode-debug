444633e1ae006f0e8c4254948f32658820002d53
RoyH11 <ruoqiuhuang@gmail.com>
2025-03-31 22:19:42 -0500

    380

--- leetcode150medium.py
+++ leetcode150medium.py
@@ -164,3 +164,52 @@ class Solution:
                 return i
             
         return 0
+    
+
+# 380
+import random
+class RandomizedSet:
+
+    def __init__(self):
+        self.vals = []
+        self.pos = {}
+
+
+    def insert(self, val: int) -> bool:
+        if val in self.pos: 
+            return False
+        self.vals.append(val)
+        self.pos[val] = len(self.vals) - 1
+        return True
+        
+
+    def remove(self, val: int) -> bool:
+        if val not in self.pos: 
+            return False
+        last_val = self.vals[-1]
+
+        val_index = self.pos[val]
+        
+        # swap in list 
+        self.vals[val_index] = last_val
+
+        # swap in dictionary
+        self.pos[last_val] = val_index
+
+        # remove list 
+        self.vals.pop()
+
+        # remove dictionary  
+        del self.pos[val]
+
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.vals)
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
