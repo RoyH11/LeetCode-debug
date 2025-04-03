9e66f749e2ab4c3c43f26aff81d6e5a8aef8c36f
RoyH11 <ruoqiuhuang@gmail.com>
2025-04-02 21:11:07 -0500

    hard init

---
+++ leetcode150hard.py
@@ -0,0 +1,33 @@
+#135 
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        # find lowest
+        worst_kid = 0
+        for i in range(len(ratings)): 
+            if ratings[i] <= ratings[worst_kid]: 
+                worst_kid = i 
+
+        total = 1
+        if worst_kid == 0: 
+            # move right 
+            prev = 1
+            for i in range(1, len(ratings)): 
+                if ratings[i] > ratings[i-1]: 
+                    prev += 1
+                if ratings[i] <= ratings[i-1]: 
+                    prev = 1
+                total += prev
+                
+
+        elif worst_kid == len(ratings) - 1: 
+            # move left 
+            prev = 1 
+            for i in range(len(ratings) - 2, -1, -1): 
+                if ratings[i] > ratings[i+1]: 
+                    prev += 1
+                if ratings[i] <= ratings[i+1]: 
+                    prev = 1
+                total += prev
+                
+        else: 
+            # move both way 
