e1db798a0193f647b677d0f093eb63f3d04ddc90
RoyH11 <ruoqiuhuang@gmail.com>
2025-04-08 00:03:23 -0500

    933

--- leetcode150_easy.py
+++ leetcode150_easy.py
@@ -1082,3 +1082,34 @@ class Solution:
             else: 
                 return False
         return True
+    
+# 933
+from collections import deque
+class RecentCounter:
+
+    def __init__(self):
+        self.queue = deque()
+
+    def ping(self, t: int) -> int:
+        self.queue.append(t)
+        low = t - 3000
+        while self.queue[0] < low: 
+            self.queue.popleft()
+        return len(self.queue)
+
+from collections import deque
+class RecentCounter:
+
+    def __init__(self):
+        self.queue = deque()
+
+    def ping(self, t: int) -> int:
+        self.queue.append(t)
+        while self.queue and self.queue[0] < t-3000: 
+            self.queue.popleft()
+        return len(self.queue)
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
