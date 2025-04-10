0035666f21a15de8af3cde90656fa57b5eeea219
RoyH11 <ruoqiuhuang@gmail.com>
2025-04-10 17:49:21 -0500

    700

--- leetcode150_easy.py
+++ leetcode150_easy.py
@@ -1199,3 +1199,35 @@ class Solution:
                 yield from dfs(root.right)
 
         return list(dfs(root1)) == list(dfs(root2))
+    
+# 700
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root is None: 
+            return None
+        
+        if root.val == val: 
+            return root
+        
+        if val < root.val: 
+            return self.searchBST(root.left, val)
+        else: 
+            return self.searchBST(root.right, val) 
+
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        while root: 
+            if root.val == val: 
+                return root
+            elif val < root.val: 
+                root = root.left
+            else: 
+                root = root.right
+
+        return None
