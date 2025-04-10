7e3b6f84c7f5d86b82d306d7df4572c28085920d
RoyH11 <ruoqiuhuang@gmail.com>
2025-04-10 18:12:00 -0500

    1137

--- leetcode150_easy.py
+++ leetcode150_easy.py
@@ -1268,4 +1268,23 @@ class Solution:
                 high = mid - 1
             else: 
                 low = mid + 1
-                
+
+# 1137
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        nums = [0, 1, 1]
+        
+        for i in range(3, n+1): 
+            nums.append(nums[i-1] + nums[i-2] + nums[i-3])
+
+        return nums[n]
+    
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0: return 0
+        if n in (1, 2): return 1
+
+        a, b, c = 0, 1, 1
+        for _ in range(3, n+1): 
+            a, b, c = b, c, a+b+c
+        return c
