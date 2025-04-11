c2f2384605c32a71fbd4ce59ffff674d9a3f71d0
RoyH11 <ruoqiuhuang@gmail.com>
2025-04-11 17:05:31 -0500

    1679

--- leetcode150_medium.py
+++ leetcode150_medium.py
@@ -794,3 +794,21 @@ class Solution:
         return write
 
 # 1679
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        left = 0
+        right = len(nums) - 1
+        output = 0
+
+        while left < right: 
+            if nums[left] + nums[right] == k: 
+                output += 1
+                left += 1
+                right -= 1
+            elif nums[left] + nums[right] < k: 
+                left += 1
+            else: 
+                right -= 1
+
+        return output
