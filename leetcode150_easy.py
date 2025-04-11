7f6022393ebd0731d6d5bd31c8a082b61ad909c5
RoyH11 <ruoqiuhuang@gmail.com>
2025-04-10 20:10:39 -0500

    338

--- leetcode150_easy.py
+++ leetcode150_easy.py
@@ -1304,3 +1304,37 @@ class Solution:
         for i in range(2, len(cost)+1): 
             a, b = b, min(a + cost[i-2], b + cost[i-1])
         return b
+    
+# 338 
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = []
+        for i in range(n+1): 
+            bits = 0
+            num = i
+            while num != 0: 
+                bits += num % 2
+                num //= 2
+            ans.append(bits)
+        return ans
+    
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0]
+        for i in range(1, n+1): 
+            ans.append(ans[i>>1] + (i&1))
+        return ans
+    
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n+1)
+        for i in range(1, n+1): 
+            ans[i] = ans[i>>1] + (i&1)
+        return ans
+    
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n+1)
+        for i in range(1, n+1): 
+            ans[i] = ans[i//2] + (i%2)
+        return ans
