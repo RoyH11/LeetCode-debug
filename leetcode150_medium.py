b8a644694f54b14642c7efdb413d95c72af35b31
RoyH11 <ruoqiuhuang@gmail.com>
2025-04-14 20:56:07 -0500

    394

--- leetcode150_medium.py
+++ leetcode150_medium.py
@@ -1007,3 +1007,28 @@ class Solution:
 
         return stack
                 
+# 394 
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        current_string = ''
+        current_num = 0
+
+        for c in s: 
+            if c.isdigit(): 
+                current_num = current_num * 10 + int(c)
+
+            elif c == '[': 
+                stack.append((current_string, current_num))
+                current_string = ''
+                current_num = 0
+
+            elif c == ']': 
+                prev_string, repeat = stack.pop()
+                current_string = prev_string + current_string * repeat
+
+            else: 
+                current_string += c
+
+        return current_string
+
