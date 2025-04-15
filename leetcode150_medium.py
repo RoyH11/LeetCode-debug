1de7fa7a31a43d63ba7d4f28f7b1a3e3c903fbb9
RoyH11 <ruoqiuhuang@gmail.com>
2025-04-14 21:48:23 -0500

    649

--- leetcode150_medium.py
+++ leetcode150_medium.py
@@ -1032,3 +1032,44 @@ class Solution:
 
         return current_string
 
+# 649
+from collections import deque
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        queue = deque(senate)
+        while queue: 
+            current_senate = queue.popleft()
+            if current_senate == 'D': 
+                if 'R' in queue: 
+                    queue.remove('R')
+                else:
+                    return 'Dire'
+            else: 
+                if 'D' in queue:
+                    queue.remove('D')
+                else: 
+                    return 'Radiant'
+            queue.append(current_senate)
+
+from collections import deque
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        n = len(senate)
+        radiant = deque()
+        dire = deque()
+        for i, s in enumerate(senate): 
+            if s == 'R': 
+                radiant.append(i)
+            else: 
+                dire.append(i)
+        
+        while radiant and dire: 
+            r_idx = radiant.popleft()
+            d_idx = dire.popleft()
+
+            if r_idx < d_idx: 
+                radiant.append(r_idx + n)
+            else: 
+                dire.append(d_idx + n)
+
+        return 'Radiant' if radiant else 'Dire'
