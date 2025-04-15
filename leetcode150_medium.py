7a4694338f4da35f46cf0bd4d3f017536651cdab
RoyH11 <ruoqiuhuang@gmail.com>
2025-04-14 20:04:13 -0500

    735

--- leetcode150_medium.py
+++ leetcode150_medium.py
@@ -988,3 +988,22 @@ class Solution:
             else: 
                 stack.pop()
         return ''.join(stack)
+
+# 735 
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for a in asteroids: 
+            if a > 0: 
+                stack.append(a)
+            else: 
+                while stack and 0 < stack[-1] < -a: 
+                    stack.pop()
+
+                if not stack or stack[-1] < 0: 
+                    stack.append(a)
+                elif stack[-1] == -a: 
+                    stack.pop()
+
+        return stack
+                
