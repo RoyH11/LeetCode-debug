b79838dd38714e5b40117188073cc4cdec1fd2ea
RoyH11 <ruoqiuhuang@gmail.com>
2025-04-17 21:24:40 -0500

    328

--- leetcode150_medium.py
+++ leetcode150_medium.py
@@ -1122,3 +1122,43 @@ class Solution:
 
         prev.next = slow.next
         return head  
+    
+# 328
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None or head.next == None or head.next.next == None: 
+            return head 
+        
+        even = head 
+        odd = head.next 
+        odd_head = head.next
+        while odd and odd.next: 
+            even.next = even.next.next
+            odd.next = odd.next.next
+            even = even.next
+            odd = odd.next 
+
+        even.next = odd_head
+        return head
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head: 
+            return head 
+        
+        even = head 
+        odd = head.next 
+        odd_head = head.next
+        while odd and odd.next: 
+            even.next = even.next.next
+            odd.next = odd.next.next
+            even = even.next
+            odd = odd.next 
+
+        even.next = odd_head
+        return head
