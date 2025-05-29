e3382b6e567debb97e781c45570ae359fb0304ac
RoyH11 <ruoqiuhuang@gmail.com>
2025-05-29 11:51:38 -0500

    2336

--- leetcode_python/leetcode150_medium.py
+++ leetcode_python/leetcode150_medium.py
@@ -1753,3 +1753,34 @@ class Solution:
                 heapq.heappushpop(min_heap, num)
 
         return min_heap[0]
+    
+# 2336 
+import heapq
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        self.current = 1
+        self.heap = []
+        self.in_heap = set()
+
+    def popSmallest(self) -> int:
+        if self.heap:
+            smallest = heapq.heappop(self.heap)
+            self.in_heap.remove(smallest)
+            return smallest
+        else: 
+            smallest = self.current
+            self.current += 1
+            return smallest
+
+    def addBack(self, num: int) -> None:
+        if num < self.current and num not in self.in_heap: 
+            heapq.heappush(self.heap, num)
+            self.in_heap.add(num)
+        
+
+
+# Your SmallestInfiniteSet object will be instantiated and called as such:
+# obj = SmallestInfiniteSet()
+# param_1 = obj.popSmallest()
+# obj.addBack(num)
