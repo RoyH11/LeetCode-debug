0ad8948dd9f0905bd2dfac5812be758e4fefa3d2
RoyH11 <ruoqiuhuang@gmail.com>
2025-06-04 22:03:47 -0500

    162

--- leetcode_python/leetcode150_medium.py
+++ leetcode_python/leetcode150_medium.py
@@ -1945,3 +1945,35 @@ class Solution:
             results.append(m - idx)
 
         return results
+    
+#162 
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        if len(nums) == 1: 
+            return 0
+        
+        for i in range(len(nums)):
+            if i == 0: 
+                if nums[i] > nums[i + 1]: 
+                    return i
+            elif i == len(nums) - 1: 
+                return i
+            elif nums[i - 1] < nums[i] > nums[i + 1]:
+                return i
+            
+        return -1
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left < right: 
+            mid = (left + right) // 2
+
+            if nums[mid] > nums[mid + 1]: 
+                right = mid 
+            else: 
+                left = mid + 1
+
+        return left 
