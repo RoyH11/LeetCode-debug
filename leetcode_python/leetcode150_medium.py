8c2397c1bacd7ab039313afd4155cacaf37aad68
RoyH11 <ruoqiuhuang@gmail.com>
2025-06-09 23:22:39 -0500

    198

--- leetcode_python/leetcode150_medium.py
+++ leetcode_python/leetcode150_medium.py
@@ -2048,3 +2048,41 @@ class Solution:
 
         backtrack(0, [])
         return res
+
+# 198 
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums: 
+            return 0
+        
+        n = len(nums) 
+        if n == 1: 
+            return nums[0]
+        
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, n): 
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        
+        return dp[-1]
+    
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums: 
+            return 0
+        
+        n = len(nums) 
+        if n == 1: 
+            return nums[0]
+        
+        prev2 = nums[0]
+        prev1 = max(nums[0], nums[1])
+
+        for i in range(2, n): 
+            curr = max(prev1, prev2 + nums[i])
+            prev2, prev1 = prev1, curr
+        
+        return prev1
+    
