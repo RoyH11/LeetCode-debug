f368a232f800b96c941dfa2c0e98faabbfc4350e
RoyH11 <ruoqiuhuang@gmail.com>
2025-06-11 11:26:13 -0500

    790

--- leetcode_python/leetcode150_medium.py
+++ leetcode_python/leetcode150_medium.py
@@ -2128,3 +2128,40 @@ class Solution:
 
         backtrack(1, [], k, n)
         return res
+
+# 790
+class Solution:
+    def numTilings(self, n: int) -> int:
+        MOD = 10**9 + 7
+
+        if n == 0: return 1
+        if n == 1: return 1
+        if n == 2: return 2
+
+        dp = [0] * (n+1)
+        dp[0], dp[1], dp[2] = 1, 1, 2
+        presum = 0
+
+        for i in range(3, n+1): 
+            presum = (presum + dp[i-3]) % MOD
+            dp[i] = (dp[i-1] + dp[i-2] + 2 * presum) % MOD
+
+        return dp[n]
+    
+class Solution:
+    def numTilings(self, n: int) -> int:
+        MOD = 10**9 + 7
+
+        if n == 0: return 1
+        if n == 1: return 1
+        if n == 2: return 2
+
+        a, b, c = 1, 1, 2
+        presum = 0
+
+        for i in range(3, n+1): 
+            presum = (presum + a) % MOD
+            curr = (c + b + 2 * presum) % MOD
+            a, b, c = b, c, curr
+
+        return c
