57f500507261266f22e2f67b7b5815a3ad3df767
RoyH11 <ruoqiuhuang@gmail.com>
2025-06-15 13:43:56 -0500

    1143

--- leetcode_python/leetcode150_medium.py
+++ leetcode_python/leetcode150_medium.py
@@ -2195,3 +2195,36 @@ class Solution:
 
         return dp[-1] 
 
+# 1143
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n = len(text1)
+        m = len(text2)
+        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+
+        for i in range(1, n+1): 
+            for j in range(1, m+1): 
+                if text1[i-1] == text2[j-1]: 
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else: 
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        return dp[-1][-1]
+    
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        if len(text1) < len(text2): 
+            text1, text2 = text2, text1
+
+        prev = [0] * (len(text2) + 1)
+        curr = [0] * (len(text2) + 1)
+
+        for i in range(1, len(text1) + 1): 
+            for j in range(1, len(text2) + 1): 
+                if text1[i-1] == text2[j-1]: 
+                    curr[j] = prev[j-1] + 1
+                else: 
+                    curr[j] = max(prev[j], curr[j-1])
+            prev, curr = curr, [0] * (len(text2) + 1)
+        
+        return prev[-1]
