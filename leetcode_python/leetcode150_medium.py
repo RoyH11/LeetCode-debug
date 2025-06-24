a7dd1b0ce0cdebc02790a64f2e0cd482829dfa6d
RoyH11 <ruoqiuhuang@gmail.com>
2025-06-24 13:27:17 -0500

    452

--- leetcode_python/leetcode150_medium.py
+++ leetcode_python/leetcode150_medium.py
@@ -2572,3 +2572,15 @@ class Solution:
             else: 
                 count += 1
         return count 
+    
+# 452
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort(key=lambda x: x[1])
+        arrow = 0
+        prev_end = float('-inf')
+        for start, end in points: 
+            if start > prev_end: 
+                count += 1
+                prev_end = end
+        return count 
