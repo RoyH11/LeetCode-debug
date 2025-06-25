9834f2af06e8890c4e636ee2062abbb7011acdb5
RoyH11 <ruoqiuhuang@gmail.com>
2025-06-25 13:06:54 -0500

    739 readability

--- leetcode_python/leetcode150_medium.py
+++ leetcode_python/leetcode150_medium.py
@@ -2599,3 +2599,17 @@ class Solution:
             stack.append(i)
 
         return answer
+    
+# 739 refactor 
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        answer = [0] * len(temperatures)
+
+        for curr_day, curr_temp in enumerate(temperatures): 
+            while stack and curr_temp > temperatures[stack[-1]]: 
+                prev_day = stack.pop()
+                answer[prev_day] = curr_day - prev_day
+            stack.append(curr_day)
+
+        return answer
