796e834816c5d7ba4ee469ee3de93046b21680e9
RoyH11 <ruoqiuhuang@gmail.com>
2025-06-25 13:03:18 -0500

    739

--- leetcode_python/leetcode150_medium.py
+++ leetcode_python/leetcode150_medium.py
@@ -2584,3 +2584,18 @@ class Solution:
                 count += 1
                 prev_end = end
         return count 
+    
+# 739
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        stack = []
+        answer = [0] * n
+
+        for i in range(n): 
+            while stack and temperatures[i] > temperatures[stack[-1]]: 
+                prev_index = stack.pop()
+                answer[prev_index] = i - prev_index
+            stack.append(i)
+
+        return answer
