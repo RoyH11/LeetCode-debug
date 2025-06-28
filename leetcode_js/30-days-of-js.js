5a71cc922811342ae58994a8b6d5e101a5fa791b
RoyH11 <ruoqiuhuang@gmail.com>
2025-06-28 15:23:00 -0500

    2620

--- leetcode_js/30-days-of-js.js
+++ leetcode_js/30-days-of-js.js
@@ -1,3 +1,7 @@
+// 2667
+
+const { create } = require("lodash");
+
 /**
  * @return {Function}
  */
@@ -11,4 +15,31 @@ var createHelloWorld = function() {
 /**
  * const f = createHelloWorld();
  * f(); // "Hello World"
+ */
+
+// 2620 
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        n = n + 1;
+        return n - 1; 
+    };
+};
+
+const counter = createCounter(5); 
+
+console.log(counter()); // 5
+console.log(counter()); // 6
+console.log(counter()); // 7
+
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
  */
