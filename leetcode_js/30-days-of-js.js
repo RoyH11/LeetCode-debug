73a7a1c469964c5fab29431ebd2ef76dafd736aa
RoyH11 <ruoqiuhuang@gmail.com>
2025-06-30 15:07:29 -0500

    2704 and 2704 arrow

--- leetcode_js/30-days-of-js.js
+++ leetcode_js/30-days-of-js.js
@@ -43,3 +43,51 @@ console.log(counter()); // 7
  * counter() // 11
  * counter() // 12
  */
+
+// 2704
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(expected) {
+            if (val !== expected) {
+                throw new Error("Not Equal"); 
+            }
+            return true;
+        }, 
+        notToBe: function(expected) {
+            if (val === expected) {
+                throw new Error("Equal");
+            }
+            return true;
+        }
+    };
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
+
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = (val) => ({
+    
+    toBe: (expected) => {
+        if (val !== expected) {
+            throw new Error("Not Equal"); 
+        }
+        return true;
+    }, 
+    notToBe: (expected) => {
+        if (val === expected) {
+            throw new Error("Equal");
+        }
+        return true;
+    }
+
+});
