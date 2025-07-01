3471df6cb232bb15354bc454e0893792792cda7d
RoyH11 <ruoqiuhuang@gmail.com>
2025-07-01 13:26:13 -0500

    2635

--- leetcode_js/30-days-of-js.js
+++ leetcode_js/30-days-of-js.js
@@ -122,3 +122,19 @@ var createCounter = function(init) {
  * counter.reset(); // 5
  * counter.decrement(); // 4
  */
+
+// 2635 
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        result.push(fn(arr[i], i));
+    }
+
+    return result;
+};
