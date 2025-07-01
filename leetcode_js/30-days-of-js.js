72a22435d05e609c21e8e04525d8b8faa64999ad
RoyH11 <ruoqiuhuang@gmail.com>
2025-07-01 13:51:50 -0500

    2634

--- leetcode_js/30-days-of-js.js
+++ leetcode_js/30-days-of-js.js
@@ -138,3 +138,19 @@ var map = function(arr, fn) {
 
     return result;
 };
+
+// 2634
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+};
