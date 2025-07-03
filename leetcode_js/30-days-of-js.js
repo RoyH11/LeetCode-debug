e338985315bdd2c30cc8e8b6529ee6c2adfe5a09
RoyH11 <ruoqiuhuang@gmail.com>
2025-07-02 20:59:28 -0500

    promise.all

--- leetcode_js/30-days-of-js.js
+++ leetcode_js/30-days-of-js.js
@@ -307,3 +307,14 @@ var addTwoPromises = async function(promise1, promise2) {
  * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
  *   .then(console.log); // 4
  */ 
+
+//2723
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+  return Promise.all([promise1, promise2])
+    .then(([value1, value2]) => value1 + value2)
+};
