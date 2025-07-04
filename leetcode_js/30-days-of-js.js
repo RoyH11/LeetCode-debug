00c0850776233d1f179d9cdcced6296feb6b1011
RoyH11 <ruoqiuhuang@gmail.com>
2025-07-03 21:01:30 -0500

    2695

--- leetcode_js/30-days-of-js.js
+++ leetcode_js/30-days-of-js.js
@@ -1,6 +1,6 @@
 // 2667
 
-const { create } = require("lodash");
+const { create, sum } = require("lodash");
 
 /**
  * @return {Function}
@@ -471,3 +471,34 @@ Array.prototype.last = function() {
 var sortBy = function(arr, fn) {
   return arr.slice().sort((a, b) => fn(a) - fn(b));
 };
+
+// 2695
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+  this.nums = nums;
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+  return this.nums.reduce((sum, num) => sum + num, 0);
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+  return JSON.stringify(this.nums);
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
