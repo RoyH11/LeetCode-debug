c4cc154bf9202bfa167913c31585aaf5ece5b5b7
RoyH11 <ruoqiuhuang@gmail.com>
2025-07-03 22:07:37 -0500

    2694

--- leetcode_js/30-days-of-js.js
+++ leetcode_js/30-days-of-js.js
@@ -568,3 +568,57 @@ class Calculator {
     return this.result;
   }
 }
+
+// 2694
+class EventEmitter {
+  constructor() {
+    this.event = {};
+  }
+
+  /**
+     * @param {string} eventName
+     * @param {Function} callback
+     * @return {Object}
+     */
+  subscribe(eventName, callback) {
+    if (!this.event[eventName]) {
+      this.event[eventName] = [];
+    }
+    this.event[eventName].push(callback);
+
+    return {
+      unsubscribe: () => {
+        this.event[eventName] = this.event[eventName].filter(cb => cb !== callback);
+        if (this.event[eventName].length === 0) {
+          delete this.event[eventName];
+        }
+        return undefined; // Return undefined after unsubscribing
+      }
+    };
+  }
+    
+  /**
+     * @param {string} eventName
+     * @param {Array} args
+     * @return {Array}
+     */
+  emit(eventName, args = []) {
+    if (!this.event[eventName]) return [];
+
+    return this.event[eventName].map(callback => {
+      return callback(...args);
+    })
+  }
+}
+
+/**
+ * const emitter = new EventEmitter();
+ *
+ * // Subscribe to the onClick event with onClickCallback
+ * function onClickCallback() { return 99 }
+ * const sub = emitter.subscribe('onClick', onClickCallback);
+ *
+ * emitter.emit('onClick'); // [99]
+ * sub.unsubscribe(); // undefined
+ * emitter.emit('onClick'); // []
+ */
