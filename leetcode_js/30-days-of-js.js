eff2f53c9ccd24441392fe0edd49b7a13e2e9391
RoyH11 <ruoqiuhuang@gmail.com>
2025-07-03 21:05:34 -0500

    2726

--- leetcode_js/30-days-of-js.js
+++ leetcode_js/30-days-of-js.js
@@ -502,3 +502,69 @@ ArrayWrapper.prototype.toString = function() {
  * String(obj1); // "[1,2]"
  * String(obj2); // "[3,4]"
  */
+
+// 2726
+class Calculator {
+    
+  /** 
+     * @param {number} value
+     */
+  constructor(value) {
+    this.result = value;
+  }
+    
+  /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+  add(value){
+    this.result += value;
+    return this;
+  }
+    
+  /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+  subtract(value){
+    this.result -= value;
+    return this;
+  }
+    
+  /** 
+     * @param {number} value
+     * @return {Calculator}
+     */  
+  multiply(value) {
+    this.result *= value;
+    return this;
+  }
+    
+  /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+  divide(value) {
+    if (value === 0) {
+      throw new Error("Division by zero is not allowed");
+    }
+    this.result /= value;
+    return this;
+  }
+    
+  /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+  power(value) {
+    this.result **= value;
+    return this;
+  }
+    
+  /** 
+     * @return {number}
+     */
+  getResult() {
+    return this.result;
+  }
+}
