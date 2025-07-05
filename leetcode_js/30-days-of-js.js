4c1514e33f3076881299385e4d3d9540c1e2deb1
RoyH11 <ruoqiuhuang@gmail.com>
2025-07-04 19:11:45 -0500

    2637

--- leetcode_js/30-days-of-js.js
+++ leetcode_js/30-days-of-js.js
@@ -622,3 +622,28 @@ class EventEmitter {
  * sub.unsubscribe(); // undefined
  * emitter.emit('onClick'); // []
  */
+
+// 2637 
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    
+  return async function(...args) {
+    const timeoutPromise = new Promise((resuolve, reject) => {
+      setTimeout(() => reject("Time Limit Exceeded"), t);
+    })
+
+    return Promise.race([
+      fn(...args), 
+      timeoutPromise
+    ])
+  }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
