f02e93b14b6a0e434a55a88ea760434afdedcd6c
RoyH11 <ruoqiuhuang@gmail.com>
2025-07-11 21:57:25 -0500

    54

--- leetcode_python/leetcode150_medium.py
+++ leetcode_python/leetcode150_medium.py
@@ -2649,3 +2649,47 @@ class StockSpanner:
             span += self.stack.pop()[1]
         self.stack.append((price, span))
         return span
+    
+
+# 54
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        if len(matrix) == 0: 
+            return []
+        
+        result = []
+
+        top = 0
+        bottom = len(matrix) - 1
+        left = 0
+        right = len(matrix[0]) - 1
+
+        while top <= bottom and left <= right: 
+
+            # top: left to right
+            for col in range(left, right + 1): 
+                result.append(matrix[top][col])
+            top += 1
+            
+            # right: top to bottom
+            for row in range(top, bottom + 1): 
+                result.append(matrix[row][right])
+            right -= 1
+
+            # bottom: right to left
+            if top <= bottom: 
+                for col in range(right, left - 1, -1): 
+                    result.append(matrix[bottom][col])
+                bottom -= 1
+            
+            # left: bottom to top
+            if left <= right: 
+                for row in range(bottom, top - 1, -1): 
+                    result.append(matrix[row][left])
+                left += 1
+
+        return result
