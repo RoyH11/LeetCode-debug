327af328aa304eb60a1be5fdd06006169e53b8e7
RoyH11 <ruoqiuhuang@gmail.com>
2025-08-23 23:38:12 -0500

    #48 apprach 2

--- leetcode_python/leetcode150_medium.py
+++ leetcode_python/leetcode150_medium.py
@@ -2712,4 +2712,31 @@ class Solution:
         # reverse each row
         for i in range(n):
             matrix[i].reverse()
+
+# 48 apprach 2
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        if n == 1: 
+            return
         
+        # four way swap
+        for i in range(n//2):
+            for j in range(i, n-1-i):
+                # save top
+                temp = matrix[i][j]
+
+                # top = left
+                matrix[i][j] = matrix[n-1-j][i]
+
+                # left = bottom
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j]
+
+                # bottom = right
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i]
+
+                # right = top
+                matrix[j][n-1-i] = temp
